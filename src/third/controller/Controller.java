35966cd74f7e805866b8dc19555cc30e115f5054
Detreus Johnson <detreusj@gmail.com>
2019-09-04 09:15:01 -0600

    Created Stupid Project 1

---
+++ src/third/controller/Controller.java
@@ -0,0 +1,13 @@
+package third.controller;
+
+
+public class Controller
+{
+
+	public void start()
+	{
+		System.out.println("Batman is the Dark Knight");
+		System.out.print("Huey is the best Boondocks character");
+		System.out.println(" Black Magick");
+	}
+}
