30620469ed96f879ad0a69133ac6a7447fed0eb3
Detreus Johnson <detreusj@gmail.com>
2019-10-04 09:15:03 -0600

    added a return and strings

--- src/third/view/Popup.java
+++ src/third/view/Popup.java
@@ -1,6 +1,23 @@
 package third.view;
 
+import javax.swing.JOptionPane;
+
 public class Popup
 {
+	public void displayMessage(String text)
+	{
+		JOptionPane.showMessageDialog(null, text);
+	}
+
+	public String askQuestion(String text)
+	{
+		String answer = "";
+		
+		answer += JOptionPane.showInputDialog(null, text);
+		
+		return answer;
+	}
+
+
 
 }
